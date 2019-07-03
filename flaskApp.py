22ead0e423415c7fdf019c55bf70414b9bcd08a3
lintangwisesa <lintangwisesa@ymail.com>
2019-07-03 10:48:28 +0700

    flask titanic logistic

--- flaskApp.py
+++ flaskApp.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def home():
     return '<h1>Welcome!</h1>'
 
-# POST to titanic model
-@app.route('/postTitanic', methods = ['GET', 'POST'])
-def postTitanic():
+# POST 12 vars to titanic model
+@app.route('/postTitanic12', methods = ['GET', 'POST'])
+def postTitanic12():
     if request.method == 'POST':
         body = request.json
         
@@ -36,18 +36,76 @@ def postTitanic():
         print(prediksi)
         return jsonify({
             '0response' : 'POST successful!', 
-            'female' : body['female'],
-            'male' : body['male'],
-            'child' : body['child'],
-            'man' : body['man'],
-            'woman' : body['woman'],
-            'pclass' : body['pclass'],
-            'age' : body['age'],
-            'sibsp' : body['sibsp'],
-            'parch' : body['parch'],
-            'fare' : body['fare'],
-            'adultman' : body['adultman'],
-            'alone' : body['alone'],
+            'female' : female,
+            'male' : male,
+            'child' : child,
+            'man' : man,
+            'woman' : woman,
+            'pclass' : pclass,
+            'age' : age,
+            'sibsp' : sibsp,
+            'parch' : parch,
+            'fare' : fare,
+            'adultman' : adultman,
+            'alone' : alone,
+            'zPREDIKSI': int(prediksi)
+        })
+
+# POST 6 vars to titanic model
+@app.route('/postTitanic6', methods = ['GET', 'POST'])
+def postTitanic6():
+    if request.method == 'POST':
+        body = request.json
+        
+        sex = body['sex']   # female = 0, male = 1
+        age = body['age']
+        sibsp = body['sibsp']
+        parch = body['parch']
+        pclass = body['pclass']
+        fare = body['fare']
+
+        if int(sex) == 0:
+            if int(age) < 15:
+                female = 1; male = 0; child = 1 
+                man = 0; woman = 0; adultman = 0
+            else:
+                female = 1; male = 0; child = 0 
+                man = 0; woman = 1; adultman = 0
+        else:
+            if int(age) < 15:
+                female = 0; male = 1; child = 1 
+                man = 0; woman = 0; adultman = 0
+            else:
+                female = 0; male = 1; child = 0 
+                man = 1; woman = 0; adultman = 1
+        
+        if int(sibsp) == 0 and int(parch) == 0:
+            alone = 1
+        else:
+            alone = 0
+
+        # [ 0.    1.    0.    1.    0.    3.     22.    1.    0.    7.25     1.    0.  ]
+        # fm   male  child  man  woman  pclass  age  sibsp  par   fare   adultM   alone
+        prediksi = model.predict([[
+            female, male, child, man, woman, pclass, age, 
+            sibsp, parch, fare, adultman, alone
+        ]])[0]
+
+        print(prediksi)
+        return jsonify({
+            '0response' : 'POST successful!', 
+            'female' : female,
+            'male' : male,
+            'child' : child,
+            'man' : man,
+            'woman' : woman,
+            'pclass' : pclass,
+            'age' : age,
+            'sibsp' : sibsp,
+            'parch' : parch,
+            'fare' : fare,
+            'adultman' : adultman,
+            'alone' : alone,
             'zPREDIKSI': int(prediksi)
         })
 
